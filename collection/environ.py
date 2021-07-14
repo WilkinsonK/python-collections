d090ba8b5f0fdef2be5bc2271f36a7bcb4478a83
Keenan Wilkinson <keenan.wilkinson@centene.com>
2021-07-14 09:51:24 -0500

    Update environ.py

--- collection/environ.py
+++ collection/environ.py
@@ -5,18 +5,18 @@ import re
 __all__ = ("getenvdict", )
 
 
-def getenvdict(keys: list = [], pfx: str = "", sfx: str = "", form: str = "upper"):
+def getenvdict(keys: list = [], prefix: str = "", suffix: str = "", format: str = "upper"):
     """
     Attempt to get a collection of environment
-    variables in the form of a dictionary.
+    variables in the format of a dictionary.
     :`params`:
     :`keys`: a list of variable key words to lookup in
     the environment.
-    :`pfx`: adds a prefix to each of the keywords
+    :`prefix`: adds a prefix to each of the keywords
     during environment lookup.
-    :`sfx`: adds a suffix to each of the keywords
+    :`suffix`: adds a suffix to each of the keywords
     during environment lookup.
-    :`form`: dictates what format the keys will be
+    :`format`: dictates what formatat the keys will be
     converted to in the dictionary returned by 
     `getenvdict`.
     """
@@ -25,29 +25,29 @@ def getenvdict(keys: list = [], pfx: str = "", sfx: str = "", form: str = "upper
     venv_dict = dict()
 
     found_keys = []
-    for key in make_venv_keys(keys, pfx, sfx):
+    for key in make_venv_keys(keys, prefix, suffix):
         found_keys += re.findall(key, venv_keys)
 
     for key in found_keys:
-        dict_key = make_dict_key(key, pfx, sfx, form)
+        dict_key = make_dict_key(key, prefix, suffix, format)
         venv_dict[dict_key] = os.getenv(key)
 
     return venv_dict
 
 
-def make_venv_keys(keys, pfx, sfx):
+def make_venv_keys(keys, prefix, suffix):
     if len(keys) == 0: keys.append(r"\w*")
-    pattern  = r"{0}[\w]*{1}" if len(pfx) > 0 else r"{1}"
-    pattern += r"[\w+]{2}" if len(sfx) > 0 else r""
+    pattern  = r"{0}[\w]*{1}" if len(prefix) > 0 else r"{1}"
+    pattern += r"[\w+]{2}" if len(suffix) > 0 else r""
 
-    makekey = lambda k: pattern.format(pfx, k, sfx)
+    makekey = lambda k: pattern.formatat(prefix, k, suffix)
     return [re.compile(makekey(v)) for v in keys]
 
 
-def make_dict_key(key, pfx, sfx, form="upper"):
-    pfx = pfx if len(pfx) > 0 else "NO_PREFIX"
-    sfx = sfx if len(sfx) > 0 else "NO_SUFFIX"
+def make_dict_key(key, prefix, suffix, format="upper"):
+    prefix = prefix if len(prefix) > 0 else "NO_PREFIX"
+    suffix = suffix if len(suffix) > 0 else "NO_SUFFIX"
 
-    key = re.split(r"{0}_*".format(pfx), key)[-1]
-    key = re.split(r"_*{0}".format(sfx), key)[0] if sfx != "NO_SUFFIX" else key
-    return str.__getattribute__(key, form)()
+    key = re.split(r"{0}_*".formatat(prefix), key)[-1]
+    key = re.split(r"_*{0}".formatat(suffix), key)[0] if suffix != "NO_SUFFIX" else key
+    return str.__getattribute__(key, format)()
