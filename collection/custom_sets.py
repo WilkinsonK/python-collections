3c7e988f4230d3c2602f44dc98313e9bb298c49a
Keenan Wilkinson <keenanwilkinson@outlook.com>
2021-10-02 21:39:18 -0500

    custom sets

--- collection/custom_sets.py
+++ collection/custom_sets.py
@@ -2,48 +2,35 @@ import math
 from typing import Iterable
 
 
-def _get_size_buffer(iterable: Iterable):
-    size = len(iterable)
-    while is_prime(size):
-        size += 1
-    return size
-
-
-def _get_dimensions(size: int, bias: float):
-    factors = factor_smallest(size)
-    bias    = math.floor(len(factors) * bias)
-    return [math.prod(i) for i in (factors[:bias], factors[bias:])]
-
-
-def _partition_items(items: list, capacity: int):
-    new = []
-    while len(items) > 0:
-        i, items = items[:capacity], items[capacity:]
-        new.append(i)
-    return new
-
-
 class PartitionedTuple(tuple):
+    _partition_bias:     float
     _partition_count:    int
     _partition_capacity: int
 
-    def __new__(cls, items: Iterable, bias: float = 0.5, *args, **kwargs):
+    def __new__(cls, items, bias=0.5, *args, **kwargs):
         items = cls._partition_items(list(items), bias)
-        inst  = cls._make_new(items, *args, **kwargs)
+        inst  = cls._initialize_tuple(items, *args, **kwargs)
         return inst
 
-    def __init__(self, *items):
+    def __init__(self, items: Iterable, bias: float = 0.5):
         self._partition_count    = len(self)
         self._partition_capacity = len(self[0])
 
+    def __repr__(self):
+        count, capacity  = self.count, self.capacity
+        return "".join([
+            f"{self.__class__.__name__}(",
+            f"{count=}, {capacity=}, ",
+            f"[{_render_repr(self)}]", f")"
+        ])
+
     @classmethod
-    def _make_new(cls, items: list, init=True, *args, **kwargs):
+    def _initialize_tuple(cls, items: list, *args, **kwargs):
         if cls is tuple:
             cls = PartitionedTuple
 
         inst = tuple.__new__(cls, items)
-        if init:
-            inst._init(*items, *args, **kwargs)
+        inst.__init__(items, *args, **kwargs)
         return inst
 
     @classmethod
@@ -68,30 +55,66 @@ class PartitionedTuple(tuple):
         return self._partition_capacity
 
 
-def factor_smallest(num: int):
-    factors = list()
+def _factors(start: int):
+    while not (_is_prime(start) or (start <= 1)):
+        for prime in _primes(start):
+            if start % prime == 0:
+                yield prime
+                start = start // prime
+    yield start
 
-    while not (is_prime(num) or (num <= 1)):
-        for prime in primes(num):
-            if num % prime == 0:
-                factors.append(prime)
-                num = num // prime
 
-    factors.append(num)
+def _factor_smallest(num: int):
+    factors = [i for i in _factors(num)]
     return factors
 
 
-def primes(limit: int):
+def _get_size_buffer(iterable: Iterable):
+    size = len(iterable)
+    while _is_prime(size):
+        size += 1
+    return size
+
+
+def _get_dimensions(size: int, bias: float):
+    factors = _factor_smallest(size)
+    bias    = math.floor(len(factors) * bias)
+    return [math.prod(i) for i in (factors[:bias], factors[bias:])]
+
+
+def _partition_items(items: list, capacity: int):
+    new = []
+    while len(items) > 0:
+        i, items = items[:capacity], items[capacity:]
+        new.append(i)
+    return new
+
+
+def _primes(limit: int):
     next_val = 0
     while next_val < limit:
-        while not is_prime(next_val):
+        while not _is_prime(next_val):
             next_val += 1
         if next_val >= limit: break
         yield next_val
         next_val += 1
 
 
-def is_prime(num: int):
+def _render_repr(ptuple: PartitionedTuple):
+    sample = list(ptuple[0])
+    if ptuple.capacity >= 5:
+        return _render_repr_sample(sample[:5], "...")
+    return _render_repr_sample(sample)
+
+
+def _render_repr_sample(sample: list, end: str = None):
+    sample = [f"{i!r}" for i in sample]
+    if end:
+        sample += [end]
+    return ", ".join(sample)
+
+
+def _is_prime(num: int):
     if (num <= 1): return False
     if (num <= 3): return True
     if (num % 2 == 0 or num % 3 == 0): return False
@@ -103,7 +126,3 @@ def is_prime(num: int):
         step += 6
 
     return True
-
-
-part_t = PartitionedTuple([0 for _ in range(49)])
-print(part_t.capacity, part_t.count, part_t, end="\n")
